d23f2d731806aa66db5b390ab687cda465daa411
andreastunggono <andreastunggono.at@gmail.com>
2022-02-03 14:46:23 +0700

    g

---
+++ loginadmin.js
@@ -0,0 +1,6 @@
+var user = document.querySelector("#user");
+user.addEventListener("keyup", function () {
+  var u_times = document.querySelector(".u_times");
+  var u_check = document.querySelector(".u_check");
+  if ()
+});
